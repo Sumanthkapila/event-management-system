40211befa2a6e6d282a4b6a28d0b493dbd89159e
sumanth kapila <sumanthkapila@gmail.com>
2025-03-28 23:20:15 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARaxDNFOn4TF4ga0lpgWe0V86BWWcS210",
-  authDomain: "eventmanager-e5e27.firebaseapp.com",
-  projectId: "eventmanager-e5e27",
-  storageBucket: "eventmanager-e5e27.appspot.com",
-  messagingSenderId: "751583692513",
-  appId: "1:751583692513:web:584a81a20cbf2ef737d9a0",
+  apiKey: "use-your-API-Key-Here",
+  authDomain: "your_auth_domain",
+  projectId: "your_project_id",
+  storageBucket: "your_storage_bucket",
+  messagingSenderId: "your_messaging_sender_id",
+  appId: "your_app_id",
 };
 
 // Initialize Firebase
